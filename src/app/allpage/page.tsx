42914e8ad1b40d9b178c987813059e978725c9c7
IQRAKHAN2998 <iqrakhan2998@gmail.com>
2024-12-25 17:58:33 +0500

    changes

--- src/app/allpage/page.tsx
+++ src/app/allpage/page.tsx
@@ -10,7 +10,7 @@ import Wrapper from '../wrapper/wrapper'
     <div className='p-10'>
     <h2 className=' py-4 space-y-7'>{Data.map((item)=>{
       return(
-        <div key={item.id} className='border hover:scale-105 hover:bg-blue-400 shadow-xl'>
+        <div key={item.id} className='border hover:scale-105 hover:bg-slate-600 shadow-xl'>
           <Link href={`./data/${item.id}`}>
           <Image src={item.image} alt="images" width={500} height={400}/>
           <h2 className='p-5'> {item.tittle}</h2>
