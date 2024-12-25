bac372c7b50d91f9f20af6fdedaa843e9b486078
IQRAKHAN2998 <iqrakhan2998@gmail.com>
2024-12-26 01:37:11 +0500

    changes

--- src/app/wrapper/comment.tsx
+++ src/app/wrapper/comment.tsx
@@ -22,10 +22,10 @@ const Comment = () => {
         <div>
             <span className='flex flex-col'>
                 <h2 className='black font-semibold text-xl'>comment here</h2>
-                <form className='py-7 space-x-8  border-black' onSubmit={submitcommit}> 
+                <form className='py-7 space-x-8 space-y-4  border-black' onSubmit={submitcommit}> 
                     <input type="text" placeholder='Comments here' value={comment}  onChange={(notrefresh) => setComment(notrefresh.target.value)}
                      className='py-6 px-4 border border-black' />
-                    <button className= 'bg-black mt-3 text-white font-semibold rounded-md py-3 px-6'>Submit</button>
+                    <button className= 'bg-black  text-white font-semibold rounded-md py-3 px-6'>Submit</button>
                 </form>
                 <ul>{
                     comments.map((herecomment, index) => {
